d0a715842f1e2ee5b8507772b246a1e5e8798a6b
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-03-22 19:37:05 -0300

    track_mapper impl

---
+++ gps/graph/node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, id, x, y):
+        self.id = id
+        self.x = 0
+        self.y = 0
--- gps/track_mapper.py
+++ gps/track_mapper.py
@@ -1,31 +1,38 @@
 # this creates the list of nodes that is saved in the track mapping class
 import cv2
 
+from gps.graph.node import Node
+
 
 class TrackMapper:
     def define_nodes(self, initial_track_image):
-        pass
+        return self.get_click_coordinates(initial_track_image)
 
     def get_click_coordinates(self, image_path):
-        # Read the image
+        nodes_list = []
         image = cv2.imread(image_path)
         if image is None:
             print("Error: Unable to read the image.")
             return
 
-        # Create a copy of the image for displaying clicked points
         image_with_points = image.copy()
 
-        # Create a callback function to handle mouse events
+        click_counter = 1
+
         def mouse_callback(event, x, y, flags, param):
-            nonlocal image_with_points
+            nonlocal image_with_points, click_counter, nodes_list
             if event == cv2.EVENT_LBUTTONDOWN:
-                # Draw a circle at the clicked point
-                cv2.circle(image_with_points, (x, y), 5, (0, 0, 255), -1)
+                # Draw a circle at the clicked point with number inside
+                cv2.circle(image_with_points, (x, y), 30, (0, 0, 255), 2)
+                cv2.putText(image_with_points, str(click_counter), (x - 5, y + 5),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                 # Display the image with the drawn point
                 cv2.imshow("Image", image_with_points)
                 # Print the coordinates of the clicked point
                 print(f"Clicked at: ({x}, {y})")
+                # Increment the click counter
+                click_counter += 1
+                nodes_list.append(Node(click_counter, x, y))
 
         # Create a window and display the original image
         cv2.imshow("Image", image)
@@ -33,8 +40,11 @@ class TrackMapper:
         # Set the mouse callback function for the window
         cv2.setMouseCallback("Image", mouse_callback)
 
-        # Wait for the user to click on the image
-        cv2.waitKey(0)
+        while True:
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
 
         # Close all OpenCV windows
         cv2.destroyAllWindows()
+        return nodes_list
