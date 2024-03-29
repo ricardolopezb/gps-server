019d32a74f74df2f06b50191d52478ec1ec389ee
ricardolopezb <ricardolopezbonaguro@gmail.com>
2024-03-29 12:15:38 -0300

    shortest_path algorithm

--- gps/graph/node.py
+++ gps/graph/node.py
@@ -1,5 +1,9 @@
 class Node:
     def __init__(self, id, x, y):
         self.id = id
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
+        self.neighbors = []
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.append(neighbor)
--- gps/track_mapping.py
+++ gps/track_mapping.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from scipy.spatial import KDTree
 from gps.coordinates_provider import CoordinatesProvider
 
@@ -13,7 +15,28 @@ class TrackMapping:
         if result is None:
             return None
         x, y = result
+        return self.get_closest_node(x, y)
+
+    def get_closest_node(self, x, y):
         _, index = self.kd_tree.query([(x, y)])
         closest_index = index[0]
         closest_node = self.nodes[closest_index]
         return closest_node
+
+    def shortest_path(self, start_id, target_id):
+        # Initialize a queue for BFS
+        queue = deque([(start_id, [start_id])])
+        visited = set()  # Set to keep track of visited nodes
+
+        while queue:
+            node_id, path = queue.popleft()
+            node = next(filter(lambda node: node.id == node_id, self.nodes), None)
+            if node_id == target_id:
+                return path
+            if node_id not in visited:
+                visited.add(node_id)
+                for neighbor_id in node.neighbors:
+                    queue.append((neighbor_id, path + [neighbor_id]))
+
+        # If no path found
+        return None
